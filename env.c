b8d518ca195655baa58555c7c847a7f9eb15cdfb
Mathieu7483 <11436@holbertonstudents.com>
2025-08-13 10:34:28 +0200

    Format output for environment variable addresses in main function

--- env.c
+++ env.c
@@ -8,18 +8,24 @@
 */
 int main(int ac, char **av, char **env)
 {
-printf("Adresse de la variable locale 'env' (le 3e paramètre de main) : %p\n", (void *)env);
-printf("Adresse de la variable globale 'environ' : %p\n", (void *)environ);
+printf("Adresse de la variable locale 'env' (le 3e paramètre de main) :\n"
+	   "%p\n", (void *)env);
+printf("Adresse de la variable globale 'environ' :\n"
+	   "%p\n", (void *)environ);
 
 printf("\n");
 
-printf("Adresse de la première variable du tableau 'env' : %p\n", (void *)env[0]);
-printf("Adresse de la première variable du tableau 'environ' : %p\n", (void *)environ[0]);
+printf("Adresse de la première variable du tableau 'env' :\n"
+	   "%p\n", (void *)env[0]);
+printf("Adresse de la première variable du tableau 'environ' :\n"
+	   "%p\n", (void *)environ[0]);
 
 printf("\n");
 
-printf("La première variable d'environnement (via 'env') : %s\n", env[0]);
-printf("La première variable d'environnement (via 'environ') : %s\n", environ[0]);
+printf("La première variable d'environnement (via 'env') :\n"
+	   "%s\n", env[0]);
+printf("La première variable d'environnement (via 'environ') :\n"
+	   "%s\n", environ[0]);
 
 return (0);
 }
