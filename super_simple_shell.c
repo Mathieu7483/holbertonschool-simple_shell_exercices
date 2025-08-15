d5a4006a7321af2c3c9e71410b90e8d481886623
Mathieu7483 <11436@holbertonstudents.com>
2025-08-15 10:53:20 +0200

    Refactor super_simple_shell.c: Improve code readability and error handling in functions

--- super_simple_shell.c
+++ super_simple_shell.c
@@ -1,4 +1,4 @@
-#include "split.h"
+#include "simple_shell.h"
 /**
 * free_args - Frees a dynamically allocated array of strings.
 * @args: The array of strings to free.
@@ -9,10 +9,10 @@ void free_args(char **args)
 
 	if (args == NULL)
 		return;
-	while (args[i] != NULL)
+	while (args[i] != NULL)/* Free each string in the array */
 	{
-		free(args[i]);
-		i++;
+		free(args[i]);  /* Free the current string */
+		i++;            /* Move to the next string */
 	}
 	free(args);
 }
@@ -27,9 +27,10 @@ char *read_line(void)
 
 	size_t bufsize = 0;
 
+	/*Read a line from stdin(keyboard), getline will allocate memory as needed*/
 	if (getline(&line, &bufsize, stdin) == -1)
 	{
-		if (feof(stdin))
+		if (feof(stdin)) /*Check if end-of-file (Ctrl+D) */
 		{
 			free(line);
 			return (NULL);
@@ -37,7 +38,7 @@ char *read_line(void)
 		else
 		{
 			free(line);
-			perror("getline");
+			perror("getline"); /* Print error message for getline failure */
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -52,41 +53,40 @@ char *read_line(void)
 char **parse_line(char *line)
 {
 	char **tokens = NULL;
-
 	char *token;
-
 	int bufsize = 64;
-
 	int i = 0;
 
+/*Allocate memory to store pointers to tokens*/
 	tokens = malloc(bufsize * sizeof(char *));
-	if (!tokens)
+	if (tokens == NULL) /* Check if memory allocation failed */
 	{
-		perror("malloc");
+		perror("malloc");/*print error on Malloc*/
 		exit(EXIT_FAILURE);
 	}
-	token = strtok(line, " \t\r\n\a");
+	token = strtok(line, " \t\r\n\a");/* Split the line into tokens*/
 	while (token != NULL)
 	{
-		tokens[i] = strdup(token);
-		if (!tokens[i])
+		tokens[i] = strdup(token);/*Make a copy of the token using strdup*/
+		if (tokens[i] == NULL)
 		{
 			free_args(tokens);
 			perror("strdup");
 			exit(EXIT_FAILURE);
 		}
 		i++;
-		if (i >= bufsize)
+		if (i >= bufsize) /*Check if we need more space for tokens*/
 		{
-			bufsize += 64;
+			bufsize += 64; /*Increase the buffer size to accommodate more tokens*/
 			tokens = realloc(tokens, bufsize * sizeof(char *));
-			if (!tokens)
+			/*Resize the tokens array to hold more entries */
+			if (tokens == NULL)
 			{
 				perror("realloc");
 				exit(EXIT_FAILURE);
 			}
 		}
-		token = strtok(NULL, " \t\r\n\a");
+		token = strtok(NULL, " \t\r\n\a"); /*Get the next token*/
 	}
 	tokens[i] = NULL;
 	return (tokens);
@@ -102,29 +102,32 @@ int execute_command(char **args)
 	pid_t pid;
 	int status;
 
-	if (args == NULL || args[0] == NULL)
+	if (args == NULL || args[0] == NULL)/*Check if no command was entered*/
 		return (1);
 
-	pid = fork();
+	pid = fork(); /* Create a new process */
 	if (pid == 0)
 	{
+		/*Execute the command using execvp, which searches the PATH*/
 		if (execvp(args[0], args) == -1)
 		{
 			perror("execvp");
 			exit(EXIT_FAILURE);
 		}
 	}
-	else if (pid < 0)
+	else if (pid < 0) /*Check if fork failed*/
 	{
 		perror("fork");
 		return (1);
 	}
 	else
 	{
+		/*Wait for the child process to complete and store its exit status*/
 		waitpid(pid, &status, 0);
 	}
 
 	return (WEXITSTATUS(status));
+	/*Return the exit status of the child process*/
 }
 
 /**
@@ -146,8 +149,8 @@ int main(int argc, char **argv)
 
 	while (1)
 	{
-		printf("$ ");
-		line = read_line();
+		printf("Cisfun$ ");/*Prompt*/
+		line = read_line();/* Read a line of input from the user */
 
 		if (line == NULL)
 		{
@@ -155,16 +158,16 @@ int main(int argc, char **argv)
 			break;
 		}
 
-		args = parse_line(line);
+		args = parse_line(line);/*Parse the input line into separate arguments*/
 		if (args != NULL && args[0] != NULL)
 		{
-			status = execute_command(args);
+			status = execute_command(args);/*Execute the command and get its status*/
 		}
 
-		free(line);
+		free(line); /*Free memory allocated for the input line*/
 		if (args != NULL)
-			free_args(args);
+			free_args(args); /*Free memory allocated for command arguments*/
 	}
 
-	return (status);
+	return (status); /*Return the status of the last executed command*/
 }
